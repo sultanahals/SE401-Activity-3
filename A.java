00371db6fb9fa2f191d3594cc7ac5221d1e74281
rsuhaibani365 <rsuhaibani@psu.edu.sa>
2024-09-11 08:31:44 +0300

    Create A.java

---
+++ A.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int result = add(5, 10);
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int a, int b) {
+        int sum = a + b
+        return sum;
+    }
+}
