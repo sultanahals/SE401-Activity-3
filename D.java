8aee3e33d4f72628a735db24297b63eeaaa08826
rsuhaibani365 <rsuhaibani@psu.edu.sa>
2024-09-11 08:33:03 +0300

    Create D.java

---
+++ D.java
@@ -0,0 +1,9 @@
+public class Comparison {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a = b) {
+            System.out.println("a is equal to b");
+        }
+    }
+}
