3420c232726ae0d485a4e02c4de0c8dfe6576dee
sultanahals <sultanah_550@outlook.com>
2025-09-18 00:38:16 +0300

    Update E.java

--- E.java
+++ E.java
@@ -1,6 +1,6 @@
 public class LoopExample {
-    public static void main(String[] args) 
-        for (int i = 0; i < 5; i++) 
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Iteration: " + i);
         }
     }
