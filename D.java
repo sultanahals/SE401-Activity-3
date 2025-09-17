8b7d0ac1cfe68932df42cef9c757d10d266b7ffe
sultanahals <sultanah_550@outlook.com>
2025-09-18 00:35:26 +0300

    Update D.java

--- D.java
+++ D.java
@@ -2,7 +2,7 @@ public class Comparison {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        if (a = b) {
+        if (a == b) {
             System.out.println("a is equal to b");
         }
     }
